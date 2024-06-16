6e9f4d1c78767293dbbd6c8c4583fe7ad16b1272
iswbm <wongbingming@163.com>
2024-06-16 22:52:26 +0800

    修改 baidutongji.js

--- source/_static/js/baidutongji.js
+++ source/_static/js/baidutongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?26ee367cf320d48d046542295cdb0dad";
+  hm.src = "https://hm.baidu.com/hm.js?1c00a5d8f392abda50bf926cb93939a1";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
